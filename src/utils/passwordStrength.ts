e2e4854da4ef30467c43f1607e445e28ab7413ae
bra-charles <charlesadunkansah55@gmail.com>
2024-06-29 20:50:18 +0000

    Created a function to analyze password strength

---
+++ src/utils/passwordStrength.ts
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import {PasswordComplexityConfig} from "./generatePassword"  
+ 
+// defines password strength
+enum PasswordStrength {
+	tooWeak = 'tooWeak',
+	weak = 'weak',
+	medium = 'medium',
+	strong = 'strong',
+}
+
+function computePasswordStrength(
+	length: number,
+	config: PasswordComplexityConfig[]
+) {
+	let passwordStrengthScore = 0;
+
+	if (length >= 8) {
+		passwordStrengthScore += 1;
+	}
+
+	for (let i = 0; i < config.length; i++) {
+		let currentConfig = config[i];
+		if (currentConfig.value) {
+			passwordStrengthScore += 1;
+		}
+	}
+
+	let strength: PasswordStrength;
+	switch (passwordStrengthScore) {
+		case 0:
+		case 1:
+			strength = PasswordStrength.tooWeak;
+			break;
+		case 2:
+			strength = PasswordStrength.weak;
+			break;
+		case 3:
+		case 4:
+			strength = PasswordStrength.medium;
+			break;
+		default:
+			strength = PasswordStrength.strong;
+	}
+
+	return strength;
+}
+
+// console.log(computePasswordStrength(5, config));
+
